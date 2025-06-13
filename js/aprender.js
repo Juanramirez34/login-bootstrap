024cfd842783bcaae712e4f0c8a0e09e5bf7bcb9
Juanramirez34 <ramirezortizj76@gmail.com>
2025-06-13 11:05:45 -0500

    login1

---
+++ js/aprender.js
@@ -0,0 +1,31 @@
+console.log("hola profe")
+//variables = let,var,const
+
+const edad = 18;
+let edad_dos = 18;
+
+edad_dos = edad_dos + 2;
+console.log(edad_dos)
+
+//tipos de datos = entero decimal , string , arrays
+
+var entero = 20;
+var decimal = 20.5;
+var string = "mi nombre es spike"
+var boolean = true //false
+console.log(boolean)
+var arrays_string = ["lun", "Mar","Mier","Jue","Vier"];
+
+console.log(arrays_string)
+//operadores aritmaticos +,-,*,/ %}
+
+//Operadores de comparacion 
+
+//Estructuras logicas
+//Inicializador 
+ for (let i = 0; i < arrays_string.length; i++) {
+  console.log(arrays_string[i])
+  Swal.fire("SweetAlert2 is working!");
+ }
+
+
